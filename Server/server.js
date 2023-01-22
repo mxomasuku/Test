b3c990d6894c34fba48d54210798d42949841726
[MxoMasuku] <64246766+mxomasuku@users.noreply.github.com>
2023-01-22 12:42:28 +0200

    Data control model completed with a minor bug, form submission giving a CORS error, front yet to be designed

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,16 @@
-const express = require('express')
-
-const app = express()
-
+var express = require('express')
+var cors = require('cors')
+var app = express()
+ 
+app.options('/products/:id', cors()) // enable pre-flight request for DELETE request
+app.delete('/products/:id', cors(), function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+ 
 app.get("/api", (req, res) =>{
-    res.json({"users" : ["user1", "user2", "user3", "user4"] })
+    res.json({ "users": [{userName: "Mxolisi", amount: 50 
+}, {userName: "Manatsa", amount: 50 
+}] })
 })
 
 app.listen(5000, () => {console.log('Server started on port 5000')})
--- testapp/src/Change.js
+++ testapp/src/Change.js
@@ -1,26 +1,37 @@
 import React, {useEffect, useState} from 'react'
+import Axios from 'axios'
 
 const Change = () => {
     const  [backendData, setBackendData] = useState([{}])
-    
+
+    // * useEffect Hook (without axios)
+    // useEffect( () => {
+    //     fetch("/api").then(
+    //      response => response.json()   
+    //     ).then(
+    //         data => {
+    //             setBackendData(data)
+    //         }
+    //     )
+    // }, [])
+
+    // ! useEffect Hook (with axios)
     useEffect( () => {
-        fetch("/api").then(
-         response => response.json()   
-        ).then(
-            data => {
-                setBackendData(data)
-            }
-        )
+        Axios.get('/api').then(res => res.data).then(data =>{setBackendData(data)}).catch( err => console.log(err))
     }, [])
 
             return(  
-                
+           // * conditional logic for rendering backend data     
             <div className='Change'>
                 {(typeof backendData.users == 'undefined') ? (
                     <p>Loading...</p>
                 ) : backendData.users.map((user, i) => (
                 
-                        <p key={i}>{user}</p>
+                    <div>
+                        <p key ={i}>{user.userName}</p>
+                        <p key ={i}> Your Change is: {user.amount}</p>
+                    </div>
+                       
                 
                 ))}
     
